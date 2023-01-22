2c06393f7f52e3358b43461e88aff21b4a9d9d0e
Aizkun <maxime.t13m@gmail.com>
2023-01-22 21:44:19 +0100

    correction langages web

--- src/components/knowledges/Languages.js
+++ src/components/knowledges/Languages.js
@@ -12,7 +12,7 @@ class Languages extends Component {
         frameworks: [
             {id: 1, value: "React", xp: 0.2},
             {id: 2, value: "Bootstrap", xp: 1},
-            {id: 3, value: "Symfony", xp: 0.2}
+            {id: 3, value: "Symfony", xp: 0.1}
         ]
     }
     render() {
